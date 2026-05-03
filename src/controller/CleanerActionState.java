9df6779d266f99dba8ca6b84500e57b4f4e69687
Andor Tóth <tothandor1@gmail.com>
2026-05-03 18:40:12 +0200

    Add StoneVommitter attachment support
    Implement functionality to purchase and attach the StoneVommitter to
    snow plows within the CleanerActionState.

--- src/controller/CleanerActionState.java
+++ src/controller/CleanerActionState.java
@@ -6,6 +6,7 @@ import attachments.IceBreaker;
 import attachments.SaltVommiter;
 import attachments.Sweeper;
 import attachments.VomitingHead;
+import attachments.StoneVommiter;
 
 public class CleanerActionState extends GameState {
     int currentSnowPlowIdx;
@@ -30,26 +31,23 @@ public class CleanerActionState extends GameState {
             case Message.BuySnowPlow buySnowPlowMsg -> {
                 cleaner.buySnowPlow(controller.getMapModel().getRandomLane());
 
-                // TODO: Valahogy jelezni, hogy sikerült-e a vásárlás
                 yield this;
             }
 
             case Message.BuyAttachment buyAttachmentMsg -> {
-                // TODO: Valahogy jelezni, hogy sikerült-e a vásárlás
                 switch (buyAttachmentMsg.type()) {
                     case DRAGON -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new Dragon());
                     case ICE_BREAKER -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new IceBreaker());
                     case SALT_VOMITTER -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new SaltVommiter());
                     case SWEEPER -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new Sweeper());
                     case VOMITING_HEAD -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new VomitingHead());
-                    // TODO: case STONE_VOMITTER -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new StoneVomitter());
+                    case STONE_VOMITTER -> cleaner.getSnowPlows().get(currentSnowPlowIdx).buyAttachment(new StoneVommiter());
                     default -> {}
                 }
                 yield this;
             }
 
             case Message.PickLane pickLaneMsg -> {
-                // TODO: return kezelése
                 cleaner.getSnowPlows().get(currentSnowPlowIdx).gotoLane(pickLaneMsg.lane(), 0);
                 if (++currentSnowPlowIdx < cleaner.getSnowPlows().size())
                     yield this;
