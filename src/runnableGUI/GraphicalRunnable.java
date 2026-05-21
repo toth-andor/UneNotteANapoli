5f8db74bccb24a89bc8b87f71e99d4b51f5903ca
Vincze Botond Miklós <120457235+sonoboti@users.noreply.github.com>
2026-05-21 21:16:21 +0200

    runnable: main->GraphicalRunnable

--- src/runnableGUI/GraphicalRunnable.java
+++ src/runnableGUI/GraphicalRunnable.java
@@ -1,9 +1,26 @@
 package runnableGUI;
 
+import controller.ControllerLogic.Controller;
+import controller.InterpreterLogic.CommandLineInterpreter;
+import controller.InterpreterLogic.ICommandLineInterpreter;
+import runnableCLI.CommandLineRunnable;
+import view.DisplayLogic.GameWindow;
+
+import javax.swing.SwingUtilities;
+
 public class GraphicalRunnable {
 
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        ICommandLineInterpreter interpreter = new CommandLineInterpreter(controller);
+        CommandLineRunnable cli = new CommandLineRunnable(interpreter, controller);
 
+        SwingUtilities.invokeLater(() -> {
+            GameWindow window = new GameWindow(controller);
+            window.setVisible(true);
+        });
+
+        cli.run();
     }
 
 }
